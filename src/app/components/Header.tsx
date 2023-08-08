31111cc8da636526c700d29dcb60dfb595b0c170
weedeej <devildjoseph666@gmail.com>
2023-08-09 01:30:40 +0800

    Changed welcom message font

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
     <div className="bg-gray-800 p-[30px] ps-[71px] pe-[109px] flex flex-row items-center justify-between">
       <h2 className="text-amber-400 text-4xl font-bold font-inter">Pokedex</h2>
       <div className="flex flex-row gap-4 items-center">
-        <span>Welcome, [UserName]</span>
+        <span className="font-inter">Welcome, [UserName]</span>
         <div className="flex flex-row items-center gap-1">
           <Avatar
             size={57}
