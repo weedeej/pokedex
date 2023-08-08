7366095f12f1cf1df509a2637f59fa4024ebf791
weedeej <devildjoseph666@gmail.com>
2023-08-09 01:27:32 +0800

    Changed main background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`bg-gray-900 ${inter.className}`}>
         <Header/>
         {children}
       </body>
