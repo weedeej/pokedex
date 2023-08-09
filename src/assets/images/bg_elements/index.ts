17c020dc4b4fce9f1fdceddba77708408051da27
weedeej <devildjoseph666@gmail.com>
2023-08-09 15:26:17 +0800

    initial bg element impl

---
+++ src/assets/images/bg_elements/index.ts
@@ -0,0 +1,21 @@
+import ball from './ball.png'
+import el120_1 from "./Group 120 1.png"
+import el121_1 from "./Group 121 1.png"
+import el122_1 from "./Group 122 1.png"
+import el123_1 from "./Group 123 1.png"
+import el125_1 from "./Group 125 1.png"
+import el126_1 from "./Group 126 1.png"
+import el127_1 from "./Group 127 1.png"
+import el128_1 from "./Group 128 1.png"
+import el130_1 from "./Group 130 (1) 1.png"
+
+export const pokeBall = ball;
+export const el120 = el120_1;
+export const el121 = el121_1;
+export const el122 = el122_1;
+export const el123 = el123_1;
+export const el125 = el125_1;
+export const el126 = el126_1;
+export const el127 = el127_1;
+export const el128 = el128_1;
+export const el130 = el130_1;
