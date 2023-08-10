e3e0937ccb6596e356a7facf16663bd8a799caf1
weedeej <devildjoseph666@gmail.com>
2023-08-10 16:00:46 +0800

    Added hover on grid cards

--- src/app/GridView.tsx
+++ src/app/GridView.tsx
@@ -6,36 +6,42 @@ const POKEMON_WITH_IMAGES = [
   {
     pk_id: 136,
     img: flareon,
+    title: "Flareon",
   },
   {
     pk_id: 25,
     img: pikachu,
+    title: "Pikachu",
   },
   {
     pk_id: 7,
     img: squirtle,
     title: "Squirtle",
-    subtitle: "#3"
   },
   {
     pk_id: 4,
     img: charmander,
+    title: "Charmander",
   },
   {
     pk_id: 1,
     img: bulbasaur,
+    title: "Bulbasaur",
   },
   {
     pk_id: 700,
     img: sylveon,
+    title: "Sylveon",
   },
   {
     pk_id: 196,
     img: espeon,
+    title: "Espeon",
   },
   {
     pk_id: 12,
     img: butterfree,
+    title: "Butterfree",
   },
 ]
 
@@ -43,8 +49,8 @@ export function GridView() {
   return (
     <div className="grid max-w-5xl w-full mb-0 grid-cols-4 text-left gap-y-10">
         {
-          POKEMON_WITH_IMAGES.map((pk) => (
-            <Card key={`pk_card_${pk.pk_id}`} {...pk}/>
+          POKEMON_WITH_IMAGES.map((pk, idx) => (
+            <Card key={`pk_card_${pk.pk_id}`} {...pk} subtitle={`#${idx+1}`}/>
           ))
         }
       </div>
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,6 +2,7 @@
 
 import { StaticImport } from "next/dist/shared/lib/get-img-props"
 import Image from 'next/image'
+import { useState } from "react"
 
 type CardProps = {
   pk_id: number,
@@ -12,12 +13,16 @@ type CardProps = {
 
 export function Card(props: CardProps) {
   const { pk_id, img, title, subtitle } = props;
+  const [isHovered, setIsHovered] = useState(false);
 
+  function toggleHover() {
+    setIsHovered((prev) => !prev)
+  }
   console.log(!!title || !!subtitle)
   return (
-    <div className="group rounded overflow-hidden relative h-[259px] w-[216px]">
+    <div className="group rounded overflow-hidden relative h-[259px] w-[216px] cursor-pointer" onMouseLeave={toggleHover}  onMouseEnter={toggleHover}>
       {
-        (!!title || !!subtitle) && (
+        isHovered && (
           <div className=" flex flex-col gap-0 justify-end left-[8px] bottom-[8px] z-10 p-4 rounded w-[202px] h-full bg-gradient-to-t from-[rgba(0,0,0,1)] from-1% to-[rgba(255,255,255,0.0)] to-40% absolute">
             <span className="text-white text-lg font-semibold">{subtitle}</span>
             <span className="text-white text-[28px] font-semibold" >{title}</span>
