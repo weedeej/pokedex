d5cc5a78eb228a92d61667ff0f365442b8ed7ff6
weedeej <devildjoseph666@gmail.com>
2023-08-12 01:02:05 +0800

    Fixed popup

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -5,24 +5,23 @@ type PopupProps = {
   onClose: () => void,
   content: JSX.Element,
   anchor: RefObject<any>,
-  position: "bottom-left" | "bottom-right";
+  position?: "bottom-left" | "bottom-right";
+  addTop?: number;
+  subtractLeft?:number;
 }
 
 export function Popup(props: PopupProps) {
-  const {isOpen, onClose, content, anchor, position} = props;
+  const {isOpen, onClose, content, anchor, position, subtractLeft, addTop} = props;
   if (!isOpen || !anchor.current) return null;
 
   const {offsetTop, offsetLeft} = anchor.current;
   const pos = {
-    top: offsetTop+44,
-    left: position === "bottom-left" ? offsetLeft-215 : offsetLeft-64
+    top: offsetTop+(addTop ?? 44),
+    left: position === "bottom-left" ? offsetLeft-(subtractLeft ?? 215) : offsetLeft-(subtractLeft ?? 64)
   }
   return (
-    <div className="absolute flex w-full h-full" onClick={onClose}>
-      <div className="flex bg-white rounded-md py-2 absolute min-w-[175px] min-h-[53px]" style={{...pos}} onClick={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-      }}>
+    <div className="absolute flex w-full h-full z-50" onClick={onClose}>
+      <div className="flex bg-white rounded-md py-2 absolute min-w-[175px] min-h-[53px]" style={{...pos}} onClick={(e) => e.stopPropagation()}>
         {content}
       </div>
     </div>
