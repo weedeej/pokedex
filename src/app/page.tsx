9e581da89b42fbe219169f2a14b16f32f9c3ddba
weedeej <devildjoseph666@gmail.com>
2023-08-12 01:07:41 +0800

    Added filter comopnents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { bulbasaur, butterfree, charmander, espeon, flareon, pikachu, squirtle, sylveon } from '@/assets/images/pokemon'
-import { IconButton } from './components'
+import { Checkbox, IconButton, Popup } from './components'
 import { FilterIcon, GridViewIcon, ListViewIcon } from '@/assets/icons'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { GridView } from './GridView';
 import { ListView } from './ListView';
 import { PkInfo } from '@/types';
@@ -68,9 +68,14 @@ const PROVIDED_POKEMON = [
   },
 ]
 
+const types = ["Normal", "Fire", "Water", "Grass", "Flying", "Fighting"];
+
 export default function Home() {
   const [viewMode, setViewMode] = useState<ViewMode>(ViewMode.GRID);
-  const [pkInfoList, setPkInfoList] = useState<(PkInfo & {img: StaticImport | null})[] | null>(null);
+  const [pkInfoList, setPkInfoList] = useState<(PkInfo & { img: StaticImport | null })[] | null>(null);
+  const [isFilterMenuOpen, setIsFilterMenuOpen] = useState<boolean>(false);
+  const [filter, setFilter] = useState(new Set());
+  const filterButtonRef = useRef(null);
 
   useEffect(() => {
     if (!!pkInfoList) return;
@@ -78,32 +83,58 @@ export default function Home() {
       const pkInfo = await getPokemon(pk.pk_id);
       if (!pkInfo) return;
       pkInfo.name = toTitleCase(pkInfo.name);
-      setPkInfoList((prev) => [...prev ?? [], {...pkInfo, img: pk.img}])
+      setPkInfoList((prev) => [...prev ?? [], { ...pkInfo, img: pk.img }])
     });
-  }, []);
+  }, [pkInfoList]);
+
+  function toggleFilterMenu() {
+    setIsFilterMenuOpen(prev => !prev);
+  }
+
+  function onFilterCheckboxChange(checked: boolean, type: string) {
+    
+    setFilter((prev) => {
+      const newFilter = new Set(prev);
+      checked ? newFilter.add(type) : newFilter.delete(type);
+      return newFilter 
+    });
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center py-12 gap-12 text-white">
+      <Popup
+        isOpen={isFilterMenuOpen}
+        onClose={toggleFilterMenu}
+        content={<div className="flex flex-col gap-2 px-4 w-full">
+          {types.map((type) => (
+            <Checkbox onChange={(v) => onFilterCheckboxChange(v,type)} checked={filter.has(type)} label={type}/>
+          ))}
+        </div>}
+        anchor={filterButtonRef}
+        position={'bottom-right'}
+        addTop={-8}
+        subtractLeft={8}
+      />
       <div className="flex flex-row z-10 max-w-5xl w-full items-center justify-between text-sm">
         <p className="font-semibold text-2xl">Choose a Pokemon</p>
         <div className='flex flex-row'>
-          <IconButton>
-            <FilterIcon/>
+          <IconButton onClick={toggleFilterMenu} ref={filterButtonRef}>
+            <FilterIcon />
           </IconButton>
           <IconButton onClick={() => setViewMode(ViewMode.LIST)}>
-            <ListViewIcon/>
+            <ListViewIcon />
           </IconButton>
           <IconButton onClick={() => setViewMode(ViewMode.GRID)}>
-            <GridViewIcon/>
+            <GridViewIcon />
           </IconButton>
         </div>
       </div>
       {
         !!pkInfoList && (
           !viewMode ? (
-            <GridView list={pkInfoList.filter((pk) => !!pk.img)}/>
+            <GridView list={pkInfoList.filter((pk) => !!pk.img)} />
           ) : (
-            <ListView list={pkInfoList}/>
+            <ListView list={pkInfoList} />
           )
         )
       }
