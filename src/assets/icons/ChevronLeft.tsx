5fed56a3e6541eebf8a6fa5e5f834cc6a8a04b76
weedeej <devildjoseph666@gmail.com>
2023-08-11 20:05:30 +0800

    Fixed icons

--- src/assets/icons/ChevronLeft.tsx
+++ src/assets/icons/ChevronLeft.tsx
@@ -3,16 +3,15 @@ import { SVGProps } from "react"
 export const ChevronLeft = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24px"
-    height="24px"
-    viewBox="0 0 24 24"
+    width={16}
+    height={16}
     fill="none"
     {...props}
   >
     <path
-      fill="#718096"
+      fill="#EDF2F7"
       fillRule="evenodd"
-      d="M5.166.234a.8.8 0 0 1 0 1.132L2.53 4l2.635 2.634a.8.8 0 1 1-1.132 1.132l-3.2-3.2a.8.8 0 0 1 0-1.132l3.2-3.2a.8.8 0 0 1 1.132 0Z"
+      d="M10.166 4.234a.8.8 0 0 1 0 1.132L7.53 8l2.635 2.634a.8.8 0 0 1-1.132 1.132l-3.2-3.2a.8.8 0 0 1 0-1.132l3.2-3.2a.8.8 0 0 1 1.132 0Z"
       clipRule="evenodd"
     />
   </svg>
--- src/assets/icons/ChevronRight.tsx
+++ src/assets/icons/ChevronRight.tsx
@@ -2,16 +2,15 @@ import { SVGProps } from "react"
 export const ChevronRight = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24px"
-    height="24px"
-    viewBox="0 0 24 24"
+    width={16}
+    height={16}
     fill="none"
     {...props}
   >
     <path
       fill="#EDF2F7"
       fillRule="evenodd"
-      d="M.834 7.766a.8.8 0 0 1 0-1.132L3.47 4 .834 1.366A.8.8 0 0 1 1.966.234l3.2 3.2a.8.8 0 0 1 0 1.132l-3.2 3.2a.8.8 0 0 1-1.132 0Z"
+      d="M5.834 11.766a.8.8 0 0 1 0-1.132L8.47 8 5.834 5.366a.8.8 0 0 1 1.132-1.132l3.2 3.2a.8.8 0 0 1 0 1.132l-3.2 3.2a.8.8 0 0 1-1.132 0Z"
       clipRule="evenodd"
     />
   </svg>
