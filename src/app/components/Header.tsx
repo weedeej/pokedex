669a7b8b5ab4265839cd7e206d432476c775ea79
weedeej <devildjoseph666@gmail.com>
2023-08-11 20:52:42 +0800

    Added logout button

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,31 @@
 "use client"
 
-import Image from "next/image";
 import avatar_head from "@/assets/images/avatar_header.png"
 import { ChevronDown } from "@/assets/icons";
-import { IconButton, Avatar } from ".";
+import { IconButton, Avatar, Popup } from ".";
+import { useRef, useState } from "react";
 
 export function Header() {
-  return (
-    <div className="bg-gray-800 p-[30px] ps-[71px] pe-[109px] flex flex-row items-center justify-between">
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  function onLogout() {
+    alert("Not implemented");
+    setIsPopupOpen(false);
+  }
+  return (<>
+    <div className="bg-gray-800 p-[30px] ps-[71px] pe-[109px] flex flex-row items-center justify-between relative">
+      <Popup
+        isOpen={isPopupOpen}
+        onClose={() => setIsPopupOpen(false)}
+        anchor={dropdownRef}
+        position="bottom-left"
+        content={(<>
+          <div className="text-red-500 px-4 flex items-center hover:bg-gray-200 w-full cursor-pointer" onClick={onLogout}>
+            Log out
+          </div>
+        </>)}
+      />
       <h2 className="text-amber-400 text-4xl font-bold">Pokedex</h2>
       <div className="flex flex-row gap-4 items-center text-white">
         <span>Welcome, [UserName]</span>
@@ -17,11 +35,11 @@ export function Header() {
             alt="avatar_head"
             img={avatar_head}
           />
-          <IconButton onClick={console.log}>
+          <IconButton onClick={() => setIsPopupOpen(true)} ref={dropdownRef}>
             <ChevronDown />
           </IconButton>
         </div>
       </div>
     </div>
-  )
+  </>)
 }
