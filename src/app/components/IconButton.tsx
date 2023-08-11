1cd6e237500e0a5246bca92e208c2fd5109d0997
weedeej <devildjoseph666@gmail.com>
2023-08-11 20:56:56 +0800

    Added forward ref on iconbutton

--- src/app/components/IconButton.tsx
+++ src/app/components/IconButton.tsx
@@ -1,15 +1,17 @@
 "use client"
 
+import { ForwardedRef, forwardRef } from "react";
+
 interface IconButtonProps extends React.HTMLProps<HTMLButtonElement> {
   onClick?: (e?: React.MouseEvent) => void;
 }
 
-export function IconButton(props: IconButtonProps) {
+export const IconButton = forwardRef(function IconButton(props: IconButtonProps, ref:ForwardedRef<any>) {
   const {onClick, children, className} = props;
 
   return (
-    <div className={`p-2 rounded-full hover:bg-gray-700 cursor-pointer ${className} items-center`} onClick={onClick}>
+    <div className={`p-2 rounded-full hover:bg-gray-700 cursor-pointer ${className} items-center`} onClick={onClick} ref={ref}>
       {children}
     </div>
   )
-}
+})
