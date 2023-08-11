0428cfc50b44599c477403ff7d5a307be7348a10
weedeej <devildjoseph666@gmail.com>
2023-08-11 20:45:17 +0800

    Added popup

---
+++ src/app/components/Popup.tsx
@@ -0,0 +1,30 @@
+import { RefObject } from "react";
+
+type PopupProps = {
+  isOpen: boolean,
+  onClose: () => void,
+  content: JSX.Element,
+  anchor: RefObject<any>,
+  position: "bottom-left" | "bottom-right";
+}
+
+export function Popup(props: PopupProps) {
+  const {isOpen, onClose, content, anchor, position} = props;
+  if (!isOpen || !anchor.current) return null;
+
+  const {offsetTop, offsetLeft} = anchor.current;
+  const pos = {
+    top: offsetTop+44,
+    left: position === "bottom-left" ? offsetLeft-96 : offsetLeft-64
+  }
+  return (
+    <div className="absolute flex w-full h-full" onClick={onClose}>
+      <div className="flex bg-white rounded-md py-2 px-4 absolute" style={{...pos}} onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}>
+        {content}
+      </div>
+    </div>
+  )
+}
